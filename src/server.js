5f89a0bfd40ff7053d18199ddf57cdc83450b342
mhdalihoria <alihoria123456@gmail.com>
2025-08-03 14:45:53 +0300

    attempt #6

--- src/server.js
+++ src/server.js
@@ -42,3 +42,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
+
+app.get('/', (req, res) => {
+  res.send("✅ Server is alive");
+});
